709fe71e7a369585373da4b19b309d27955fd495
GUSTAVO-hub-ux <gustavobonniecampos@gmail.com>
2026-05-05 17:37:00 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Atualizado para v4 para forçar o navegador a apagar a versão velha!
+const CACHE_NAME = 'chronographic-v4'; 
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); 
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response; 
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
